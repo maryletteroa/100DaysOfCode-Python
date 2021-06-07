e0752cda5073e1cd4d3b2df4f0797d024443e99d
maryletteroa <maryletteroa@gmail.com>
2021-06-07 11:51:29 +0800

    add day 4 - rock paper scissors

---
+++ 04/heads_or_tails.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Marylette B. Roa
+# @Date:   2021-06-06 11:03:26
+# @Last Modified by:   Marylette B. Roa
+# @Last Modified time: 2021-06-06 11:03:30
+
+import random
+
+head = random.randint(0,1)
+
+if head:
+  print("Heads")
+else:
+  print("Tails")
