5832034b76e3f9618c0919c52a72f5047c6e2113
maryletteroa <maryletteroa@gmail.com>
2021-07-17 16:39:56 +0800

    add day 46 - music time machine

---
+++ 46/main.py
@@ -0,0 +1,63 @@
+from warnings import resetwarnings
+from bs4 import BeautifulSoup
+import requests
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
+
+SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
+
+
+# get the top 100 songs
+
+date = input("Which year do you want to travel to? Type the date in the format YYYY-MM-DD: ")
+URL = f"https://www.billboard.com/charts/hot-100/{date}"
+
+response = requests.get(URL)
+response.raise_for_status
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+songs = soup.find_all(name="span", class_="chart-element__information__song")
+titles = [song.getText() for song in songs]
+
+# access spotify
+scope = "playlist-modify-private"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID, 
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri = "https://example.com/callback/",
+        show_dialog = True,
+        cache_path="token.txt",
+        scope=scope)
+    )
+user_id = sp.current_user()["id"]
+
+uris = []
+for title in titles:
+    # search for the songs
+    result = sp.search(title, limit=1, type="track")
+    hits = result["tracks"]["items"]
+    if len(hits) == 0:
+        print(f"The song {title} is not available in Spotify")
+    else:
+        uri = hits[0]["uri"]
+        uris.append(uri)
+
+# create a playlist
+playlist = sp.user_playlist_create(user_id, 
+    f"{date} Bilboard 100", 
+    public=False, 
+    description= f"Top songs in {date}"
+)
+print(f"Playlist '{date} Bilboard 100' created.")
+
+# add songs
+playlist_id = playlist["id"]
+external_urls = playlist["external_urls"]["spotify"]
+sp.user_playlist_add_tracks(user=user_id, playlist_id=playlist_id, tracks=uris)
+print(f"{len(uris)} songs added.")
+print(f"Access the playlist here: {external_urls}")
