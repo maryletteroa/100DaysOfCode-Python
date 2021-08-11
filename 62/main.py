e22ab77e878d826e34d8439fd6d21f75366c8ffe
maryletteroa <maryletteroa@gmail.com>
2021-08-11 15:21:04 +0800

    clean up code for day 62

--- 62/main.py
+++ 62/main.py
@@ -23,16 +23,6 @@ class CafeForm(FlaskForm):
     power_socket_availability = SelectField('Power Socket Availability', choices=power_socket_choices, validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
-
-# all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -50,7 +40,6 @@ def add_cafe():
             return redirect(url_for('add_cafe'))
     return render_template('add.html', form=form)
 
-
 @app.route('/cafes')
 def cafes():
     with open('cafe-data.csv', newline='') as csv_file:
@@ -60,7 +49,6 @@ def cafes():
             list_of_rows.append(row)
         print(list_of_rows)
     return render_template('cafes.html', cafes=list_of_rows)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
