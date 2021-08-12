7ce5b8d69cc50d3e63cde9d9fafbbfbc00ccddeb
maryletteroa <maryletteroa@gmail.com>
2021-08-12 20:10:48 +0800

    update day 63; edit typo in db uri

--- 63/main.py
+++ 63/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/ new-books-collection.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/new-books-collection.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
